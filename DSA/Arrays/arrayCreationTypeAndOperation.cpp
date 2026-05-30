9d11564b09d6796e1489b40b4c21ce374694c383
premrajanand87@gmail.com <108508635+Premraj-87@users.noreply.github.com>
2026-05-30 22:32:50 +0530

    learned about array

---
+++ DSA/Arrays/arrayCreationTypeAndOperation.cpp
@@ -0,0 +1,19 @@
+//!Today's Checklist
+
+/*
+1.Introduction to Arrays
+2.Syntax for Array Declaration
+3.Types of Arrays
+4.Basic Problems
+*/
+
+/*
+What is an Array
+ans = data structure which stores a collection of items*/
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello"<<endl;
+}
